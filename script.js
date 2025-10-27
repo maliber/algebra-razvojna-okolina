9abf88b05a1283e9a829cfd9076301f782f16d28
Matija Liber <matija.liber@gmail.com>
2025-10-27 20:10:07 +0100

    Add fixbreak from master

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const inputField = document.getElementById('inputField');
+const submitButton = document.getElementById('submitButton');
+const divValue = document.getElementById('value');
+
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const inputValue = inputField.value.trim();
+    divValue.innerHTML = '';
+    
+    const paragraph = document.createElement('p');
+    paragraph.textContent =  inputValue;
+    divValue.appendChild(paragraph);
+});
