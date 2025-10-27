da4e0fa7153ab9cd03f355cf2012fddd49124fc9
Matija Liber <matija.liber@gmail.com>
2025-10-27 19:55:43 +0100

    Add all changes to GIT repository

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 
 const inputField = document.getElementById('inputField');
+const submitButton = document.getElementById('submitButton');
+const divValue = document.getElementById('value');
+
+
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const inputValue = inputField.value.trim();
+    divValue.innerHTML = '';
+    
+    const paragraph = document.createElement('p');
+    paragraph.textContent =  inputValue;
+    divValue.appendChild(paragraph);
+});
+
+
